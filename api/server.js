f58df110cf2cd40a264c9fdbb4791c24778e0420
Henrincode <henrincode@gmail.com>
2026-03-05 21:15:08 -0300

    chore: criando var de ambiente e iniciando o node carregando as variavies do .env

---
+++ api/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+// app.get('/', (req, res) => {
+//     res.send('Olá mundo!')
+// })
+
+app.get("/", (req, res) => {
+    res.send(`Processo rodando na porta ${process.env.PORTA}`)
+})
+
+app.listen(process.env.PORTA)
